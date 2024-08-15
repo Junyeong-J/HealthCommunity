f882a0d67a5b035834956165568d9044f28312b1
JunYeong-J <eeot2827@gmail.com>
2024-08-15 15:17:39 +0900

    Feat: 회원가입 화면

---
+++ HealthCommunity/Sources/Scenes/OnBoardingScene/ViewModel/OnBoardingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OnBoardingViewModel.swift
+//  HealthCommunity
+//
+//  Created by 전준영 on 8/15/24.
+//
+
+import Foundation
---
+++ HealthCommunity/Sources/Scenes/SignUpScene/View/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  HealthCommunity
+//
+//  Created by 전준영 on 8/15/24.
+//
+
+import UIKit
+
+final class SignUpViewController: BaseViewController<SignUpView> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
