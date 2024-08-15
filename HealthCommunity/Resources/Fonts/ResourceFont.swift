2b316e8617a307465eab72d2f35b78d2979d3a78
JunYeong-J <eeot2827@gmail.com>
2024-08-15 14:21:08 +0900

    Feat: 기본으로 사용할 Font관리 파일 생성

---
+++ HealthCommunity/Resources/Fonts/ResourceFont.swift
@@ -0,0 +1,29 @@
+//
+//  ResourceFont.swift
+//  HealthCommunity
+//
+//  Created by 전준영 on 8/15/24.
+//
+
+import UIKit
+
+//MARK: - 앱에서 사용할 폰트
+enum Font {
+    static let regular13: UIFont = .systemFont(ofSize: 13)
+    static let regular14: UIFont = .systemFont(ofSize: 14)
+    static let regular15: UIFont = .systemFont(ofSize: 15)
+    static let regular16: UIFont = .systemFont(ofSize: 16)
+    static let regular17: UIFont = .systemFont(ofSize: 17)
+    static let regular18: UIFont = .systemFont(ofSize: 18)
+    static let regular19: UIFont = .systemFont(ofSize: 19)
+    static let regular20: UIFont = .systemFont(ofSize: 20)
+    
+    static let bold13: UIFont = .boldSystemFont(ofSize: 13)
+    static let bold14: UIFont = .boldSystemFont(ofSize: 14)
+    static let bold15: UIFont = .boldSystemFont(ofSize: 15)
+    static let bold16: UIFont = .boldSystemFont(ofSize: 16)
+    static let bold17: UIFont = .boldSystemFont(ofSize: 17)
+    static let bold18: UIFont = .boldSystemFont(ofSize: 18)
+    static let bold19: UIFont = .boldSystemFont(ofSize: 19)
+    static let bold20: UIFont = .boldSystemFont(ofSize: 20)
+}
