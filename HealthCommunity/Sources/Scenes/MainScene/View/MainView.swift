3a8e0e67cbf7f6fe3bca5d776b5da43df63d3240
JunYeong-J <eeot2827@gmail.com>
2024-08-18 21:38:56 +0900

    Feat: 메인뷰 생성

---
+++ HealthCommunity/Sources/Scenes/MainScene/View/MainView.swift
@@ -0,0 +1,12 @@
+//
+//  MainView.swift
+//  HealthCommunity
+//
+//  Created by 전준영 on 8/18/24.
+//
+
+import UIKit
+
+final class MainView: BaseView {
+    
+}
---
+++ HealthCommunity/Sources/Scenes/MainScene/View/MainViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewController.swift
+//  HealthCommunity
+//
+//  Created by 전준영 on 8/18/24.
+//
+
+import UIKit
+
+final class MainViewController: BaseViewController<MainView> {
+    
+}
