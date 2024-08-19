92ab7921be307cf187e8e6b07ac6fb949263ff6b
JunYeong-J <eeot2827@gmail.com>
2024-08-19 23:52:58 +0900

    Design: 앨범버튼, 글쓰기 버튼 커스텀

--- HealthCommunity/Sources/Custom/Button/CameraButton.swift
+++ HealthCommunity/Sources/Custom/Button/CameraButton.swift
@@ -5,4 +5,34 @@
 //  Created by 전준영 on 8/19/24.
 //
 
-import Foundation
+import UIKit
+
+class CameraButton: UIButton {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        configureButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension CameraButton {
+    private func configureButton() {
+        backgroundColor = .myAppWhite
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .light)
+        setImage(UIImage(systemName: "camera", withConfiguration: imageConfig)?.withTintColor(.myAppBlack, renderingMode: .alwaysOriginal), for: .normal)
+        imageView?.contentMode = .scaleAspectFit
+
+        layer.shadowColor = UIColor.black.cgColor
+        layer.masksToBounds = false
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.3
+        layer.cornerRadius = 20
+    }
+}
--- HealthCommunity/Sources/Custom/Button/PostButton.swift
+++ HealthCommunity/Sources/Custom/Button/PostButton.swift
@@ -5,4 +5,31 @@
 //  Created by 전준영 on 8/19/24.
 //
 
-import Foundation
+import UIKit
+
+class PostButton: UIButton {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        configureButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension PostButton {
+    private func configureButton() {
+        var configure = UIButton.Configuration.filled()
+        configure.baseBackgroundColor = .myAppMain
+        configure.cornerStyle = .capsule
+        configure.image = UIImage(systemName: "plus")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 20, weight: .medium))
+        self.setTitle("글쓰기", for: .normal)
+        self.configuration = configure
+        self.layer.shadowRadius = 25
+        self.layer.shadowOpacity = 0.3
+    }
+}
