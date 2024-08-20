e384048502afe4ace01ce053bdc2d28e64493cfd
JunYeong-J <eeot2827@gmail.com>
2024-08-20 23:58:00 +0900

    Feat: ReuseID 프로토콜 추가

--- HealthCommunity/Sources/Protocol/ReuseIdentifierProtocol.swift
+++ HealthCommunity/Sources/Protocol/ReuseIdentifierProtocol.swift
@@ -5,4 +5,22 @@
 //  Created by 전준영 on 8/20/24.
 //
 
-import Foundation
+import UIKit
+
+protocol ReuseIdentifierProtocol {
+    
+    static var identifier: String { get }
+    
+}
+
+extension UITableViewCell: ReuseIdentifierProtocol {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
+
+extension UICollectionViewCell: ReuseIdentifierProtocol {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
