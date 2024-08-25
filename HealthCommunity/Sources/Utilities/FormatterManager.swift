b8d67dfc421f06be481c46c9d3d989590cee3829
JunYeong-J <eeot2827@gmail.com>
2024-08-25 23:08:02 +0900

    Feat: FormatterManager 생성, 시간기간 별로 화면 표시 함수

--- HealthCommunity/Sources/Utilities/FormatterManager.swift
+++ HealthCommunity/Sources/Utilities/FormatterManager.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+class FormatterManager {
+    
+    static let shared = FormatterManager()
+    
+    private init() { }
+    
+    func formatDate(from dateString: String) -> String? {
+        let formatter = DateFormatter()
+        
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        
+        guard let date = dateFormatter.date(from: dateString) else {
+            return nil
+        }
+        
+        let currentTime = Date()
+        let duration = currentTime.timeIntervalSince(date)
+        
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day, .year], from: date, to: currentTime)
+        let daysAgo = components.day ?? 0
+        let yearsAgo = components.year ?? 0
+        
+        if duration < 60 {
+            return "방금 전"
+        } else if duration < 10 * 60 {
+            let minutesAgo = Int(duration / 60)
+            return "\(minutesAgo)분 전"
+        } else if daysAgo < 1 {
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateFormat = "a hh:mm"
+            return formatter.string(from: date)
+        } else if daysAgo < 2 {
+            return "어제"
+        } else if yearsAgo < 1 {
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateFormat = "M월 d일"
+            return formatter.string(from: date)
+        } else {
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateFormat = "yyyy.MM.dd"
+            return formatter.string(from: date)
+        }
+    }
+}
+
